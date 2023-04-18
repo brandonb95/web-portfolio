30cc99176205072b79a127283b908a68985d679b
brandonb95 <brandonbirk1995@gmail.com>
2023-04-17 23:56:36 -0700

    Bubbles can be dragged

--- brandon-birk-portfolio/src/components/Home.js
+++ brandon-birk-portfolio/src/components/Home.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "animate.css";
 import Header from "./Header";
@@ -59,6 +59,68 @@ const Home = () => {
     };
   }, []);
 
+  // let the bubbles of home be moved on mouse drag
+
+  // for dragging bubbles
+  const [dragging, setDragging] = useState(false);
+  const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
+  const bubbleRefs = useRef(Array(20).fill(null));
+  const [draggingIndex, setDraggingIndex] = useState(null);
+
+  const setBubbleRef = (index) => (element) => {
+    bubbleRefs.current[index] = element;
+  };
+
+  const handleMouseDown = (event, index) => {
+    setDragging(true); // set dragging to true
+    setDraggingIndex(index);
+    const { pageX, pageY } = event;
+    const bubble = bubbleRefs.current[index - 1];
+    const { offsetLeft, offsetTop } = bubble;
+    setCoordinates({ x: pageX - offsetLeft, y: pageY - offsetTop });
+  };
+
+  // Generate a random number between 0 and max
+  const getRandomNumber = (max) => {
+    return Math.floor(Math.random() * (max + 1));
+  };
+
+  const handleMouseMove = (event) => {
+    if (draggingIndex !== null && dragging) {
+      const { pageX, pageY } = event;
+      const bubble = bubbleRefs.current[draggingIndex - 1];
+      bubble.style.left = pageX - coordinates.x + "px";
+      bubble.style.top = pageY - coordinates.y + "px";
+
+      // check if bubble is outside viewport
+      const boundingRect = bubble.getBoundingClientRect();
+      const isOutsideLeft = boundingRect.left < 0;
+      const isOutsideTop = boundingRect.top < 0;
+      const isOutsideRight = boundingRect.right > window.innerWidth;
+      const isOutsideBottom = boundingRect.bottom > window.innerHeight;
+      if (isOutsideLeft || isOutsideTop || isOutsideRight || isOutsideBottom) {
+        bubble.style.display = "none";
+      } else {
+        bubble.style.display = "block";
+      }
+    }
+  };
+
+  const handleMouseUp = () => {
+    setDragging(false); // set dragging to false
+    setDraggingIndex(null);
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, [dragging]);
+
   return (
     <>
       {isLoaded ? (
@@ -68,30 +130,17 @@ const Home = () => {
 
             <article id="home" className="landing-banner">
               <div className="bubbles">
-                <span style={{ "--i": "19" }}></span>
-                <span style={{ "--i": "15" }}></span>
-                <span style={{ "--i": "22" }}></span>
-                <span style={{ "--i": "18" }}></span>
-                <span style={{ "--i": "12" }}></span>
-                <span style={{ "--i": "21" }}></span>
-                <span style={{ "--i": "17" }}></span>
-                <span style={{ "--i": "11" }}></span>
-                <span style={{ "--i": "13" }}></span>
-                <span style={{ "--i": "24" }}></span>
-                <span style={{ "--i": "16" }}></span>
-                <span style={{ "--i": "14" }}></span>
-                <span style={{ "--i": "19" }}></span>
-                <span style={{ "--i": "15" }}></span>
-                <span style={{ "--i": "22" }}></span>
-                <span style={{ "--i": "18" }}></span>
-                <span style={{ "--i": "12" }}></span>
-                <span style={{ "--i": "21" }}></span>
-                <span style={{ "--i": "17" }}></span>
-                <span style={{ "--i": "11" }}></span>
-                <span style={{ "--i": "10" }}></span>
-                <span style={{ "--i": "24" }}></span>
-                <span style={{ "--i": "16" }}></span>
-                <span style={{ "--i": "14" }}></span>
+                {[...Array(20)].map((_, i) => (
+                  <span
+                    key={i}
+                    style={{
+                      "--i": i + 1,
+                      left: `${getRandomNumber(window.innerWidth - 30)}px`,
+                    }}
+                    ref={(ref) => (bubbleRefs.current[i] = ref)}
+                    onMouseDown={(event) => handleMouseDown(event, i + 1)}
+                  ></span>
+                ))}
               </div>
 
               <h1 className="landing-title animate__animated animate__bounceIn">
