01773217df25879fa6f6a1aab6bcd070a2d45af7
brandonb95 <brandonbirk1995@gmail.com>
2023-04-18 22:05:57 -0700

    Bubbles can be successfully moved around on desktop

--- brandon-birk-portfolio/src/components/Home.js
+++ brandon-birk-portfolio/src/components/Home.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useCallback, useMemo, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "animate.css";
 import Header from "./Header";
@@ -71,45 +71,53 @@ const Home = () => {
     bubbleRefs.current[index] = element;
   };
 
-  const handleMouseDown = (event, index) => {
+  const handleMouseDown = useCallback((event, index) => {
     setDragging(true); // set dragging to true
     setDraggingIndex(index);
     const { pageX, pageY } = event;
     const bubble = bubbleRefs.current[index - 1];
     const { offsetLeft, offsetTop } = bubble;
     setCoordinates({ x: pageX - offsetLeft, y: pageY - offsetTop });
-  };
+  }, []);
 
   // Generate a random number between 0 and max
   const getRandomNumber = (max) => {
     return Math.floor(Math.random() * (max + 1));
   };
 
-  const handleMouseMove = (event) => {
-    if (draggingIndex !== null && dragging) {
-      const { pageX, pageY } = event;
-      const bubble = bubbleRefs.current[draggingIndex - 1];
-      bubble.style.left = pageX - coordinates.x + "px";
-      bubble.style.top = pageY - coordinates.y + "px";
+  const handleMouseMove = useCallback(
+    (event) => {
+      if (draggingIndex !== null && dragging) {
+        const { pageX, pageY } = event;
+        const bubble = bubbleRefs.current[draggingIndex - 1];
+        bubble.style.left = pageX - coordinates.x + "px";
+        bubble.style.top = pageY - coordinates.y + "px";
 
-      // check if bubble is outside viewport
-      const boundingRect = bubble.getBoundingClientRect();
-      const isOutsideLeft = boundingRect.left < 0;
-      const isOutsideTop = boundingRect.top < 0;
-      const isOutsideRight = boundingRect.right > window.innerWidth;
-      const isOutsideBottom = boundingRect.bottom > window.innerHeight;
-      if (isOutsideLeft || isOutsideTop || isOutsideRight || isOutsideBottom) {
-        bubble.style.display = "none";
-      } else {
-        bubble.style.display = "block";
+        // check if bubble is outside viewport
+        const boundingRect = bubble.getBoundingClientRect();
+        const isOutsideLeft = boundingRect.left < 0;
+        const isOutsideTop = boundingRect.top < 0;
+        const isOutsideRight = boundingRect.right > window.innerWidth;
+        const isOutsideBottom = boundingRect.bottom > window.innerHeight;
+        if (
+          isOutsideLeft ||
+          isOutsideTop ||
+          isOutsideRight ||
+          isOutsideBottom
+        ) {
+          bubble.style.display = "none";
+        } else {
+          bubble.style.display = "block";
+        }
       }
-    }
-  };
+    },
+    [coordinates, dragging, draggingIndex]
+  );
 
-  const handleMouseUp = () => {
+  const handleMouseUp = useCallback(() => {
     setDragging(false); // set dragging to false
     setDraggingIndex(null);
-  };
+  }, []);
 
   useEffect(() => {
     document.addEventListener("mousemove", handleMouseMove);
@@ -121,6 +129,29 @@ const Home = () => {
     };
   }, [dragging]);
 
+  const bubbleMouseDownHandlers = useMemo(() => {
+    return bubbleRefs.current.map(
+      (_, i) => (event) => handleMouseDown(event, i + 1)
+    );
+  }, [handleMouseDown]);
+
+  const bubbleMouseMoveHandlers = useMemo(() => {
+    return bubbleRefs.current.map(
+      (_, i) => (event) => handleMouseMove(event, i + 1)
+    );
+  }, [handleMouseMove]);
+
+  const bubbleMouseUpHandlers = useMemo(() => {
+    return bubbleRefs.current.map((_, i) => () => handleMouseUp(i + 1));
+  }, [handleMouseUp]);
+
+  const positions = useMemo(() => {
+    return [...Array(20)].map(() => ({
+      left: `${getRandomNumber(window.innerWidth - 30)}px`,
+      top: `${getRandomNumber(window.innerHeight - 30)}px`,
+    }));
+  }, []);
+
   return (
     <>
       {isLoaded ? (
@@ -130,15 +161,16 @@ const Home = () => {
 
             <article id="home" className="landing-banner">
               <div className="bubbles">
-                {[...Array(20)].map((_, i) => (
+                {positions.map((position, i) => (
                   <span
                     key={i}
                     style={{
                       "--i": i + 1,
-                      left: `${getRandomNumber(window.innerWidth - 30)}px`,
+                      left: position.left,
+                      top: position.top,
                     }}
-                    ref={(ref) => (bubbleRefs.current[i] = ref)}
-                    onMouseDown={(event) => handleMouseDown(event, i + 1)}
+                    ref={setBubbleRef(i)}
+                    onMouseDown={bubbleMouseDownHandlers[i]}
                   ></span>
                 ))}
               </div>
